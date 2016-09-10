1141269e44409f9f2bbb1389cd086e07395e913e
Leland G. Hoover <leland.g.hoover@gmail.com>
2016-09-10 15:19:13 +0000

    add loop

---
+++ src/github.com/golang/example/hello/hello.go
@@ -0,0 +1,13 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Leland")
+	for i := 0; i < 3; i++ {
+		fmt.Printf("%d \n", i)
+	}
+	fmt.Println("Again into the fog")
+}
